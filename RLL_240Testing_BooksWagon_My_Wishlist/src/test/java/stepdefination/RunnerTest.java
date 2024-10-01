fd292e2e9d39ef3affce0141b238da75e1fa46c8
dileep yadav <137282829+dileep557@users.noreply.github.com>
2024-10-01 20:53:33 +0530

    Add files via upload
    finall commit

---
+++ RLL_240Testing_BooksWagon_My_Wishlist/src/test/java/stepdefination/RunnerTest.java
@@ -0,0 +1,16 @@
+package stepdefination;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src\\test\\resources\\features\\WishListFeature2.feature",
+        glue = "stepdefination", // Adjust to your package
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+public class RunnerTest {
+	
+}
+
